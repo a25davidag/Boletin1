d7e073dab071ce97416b4939e898c41fd583544f
David Arranz González <a25davidag@teis.intranet>
2025-10-14 17:26:37 +0200

    hecho

---
+++ src/Main.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Ejercicio1
+        /*int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        int n4 = sc.nextInt();
+
+        System.out.println("El mayor es " + Math.max(n1, Math.max(n2, n3)));
+        System.out.println("El menor es " + Math.min(n1, Math.min(n2, n3)));
+        if(n1 > n2 && n1 > n3){
+            System.out.println("es el mas mayor " + n1);
+        }else if(n2 > n1 && n2 > n3) {
+            System.out.println("es mas grande " + n2);
+        } else {
+            System.out.println("Es mas grande " + n3);
+        }
+        if(n1 < n2 && n1 < n3){
+            System.out.println("mas pequeños " + n1);
+        } else if (n2 < n1 && n2 < n3) {
+            System.out.println("es mas pequeño " + n2);
+        }else {
+            System.out.println("es mas pequeños " + n3);
+        }*/
+        //Ejercicio2
+        /*for (int i = 1; i <= 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //Ejercicio 6
+        for(int j = 1;j <= 10; j++){
+            System.out.println("tabla del " + j);
+            System.out.println("**************");
+            for(int k = 1;k <= 10; k++){
+                System.out.println((j + "x" + k + "=" + j*k));
+            }
+            System.out.println("...................");
+
+        }
+        //Ejerccicio 11
+        int sumador = 0;
+        int numero = 0;
+       while(sumador <= 50){
+            if ((numero % 2) == 0) {
+                sumador++;
+                System.out.println(numero);
+            }
+            numero++;
+
+        }*/
+        //Ejercicio 12
+        int numero = 0;
+        while (numero <= 100) {
+            if ((numero % 5) != 0) {
+                System.out.println(numero);
+            }
+            numero++;
+
+        }
+    }
+}
